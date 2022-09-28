39bb3ca2eca0b0dbcbb932a3a80a5b760711b872
Matias Reyes <110213528+matias-Reye@users.noreply.github.com>
2022-09-28 20:12:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+window.addEventListener('load',()=>{
+    let button = document.querySelector('.inicioboton')
+    button.addEventListener('click', (e)=>{
+        e.preventDefault();
+        
+    })
+})
